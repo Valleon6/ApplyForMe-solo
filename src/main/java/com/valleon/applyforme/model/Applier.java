b1e2b5cd4d7baa3925e9fb5ecba794b232699c37
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-16 17:38:14 +0100

    Create Applier.java

---
+++ src/main/java/com/valleon/applyforme/model/Applier.java
@@ -0,0 +1,32 @@
+package com.valleon.applyforme.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "APPLIER")
+public class Applier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "professional_id")
+    private Professional professional;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "professional")
+    private Submission submission;
+
+}
