847a33762e6ca30772d72f778251beb0e5d62d70
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-16 19:10:00 +0100

    Create Professional.java

---
+++ src/main/java/com/valleon/applyforme/model/Professional.java
@@ -0,0 +1,41 @@
+package com.valleon.applyforme.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Professional")
+public class Professional {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @JoinColumn(name = "user_id")
+    @OneToOne (fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private User user;
+
+    @Column(name = "available_for_interview", nullable = false)
+    private boolean availableForInterview;
+
+    @OneToOne(mappedBy = "professional")
+    private Applier applier;
+
+    @OneToOne(mappedBy = "professional")
+    private ProfessionalAttachment professionalAttachment;
+
+    @OneToOne(mappedBy = "professional")
+    private ProfessionalMetadata professionalMetadata;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "professional")
+    private Submission submission;
+
+}
