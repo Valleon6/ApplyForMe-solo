7bbb6df53a8d9e742881c2b2cb70ee819e5ecbc0
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-16 21:06:16 +0100

    Create ProfessionalAttachment.java

---
+++ src/main/java/com/valleon/applyforme/model/ProfessionalAttachment.java
@@ -0,0 +1,35 @@
+package com.valleon.applyforme.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PROFESSIONAL_ATTACHMENT")
+public class ProfessionalAttachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "passport_link", nullable = false)
+    private String passportLink;
+
+    @Column(name = "resume_link", nullable = false)
+    private String resumeLink;
+
+    @Column(name = "cover_letter_link", nullable = false)
+    private String coverLetterLink;
+
+    @JoinColumn(name = "professional_id")
+    @OneToOne(fetch = FetchType.LAZY)
+    private Professional professional;
+
+}
