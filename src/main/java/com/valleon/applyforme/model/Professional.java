58db88260b1caa9ed22fba4164a49c5d7e67949b
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2023-06-26 22:47:30 +0100

    Update Professional.java

--- src/main/java/com/valleon/applyforme/model/Professional.java
+++ src/main/java/com/valleon/applyforme/model/Professional.java
@@ -1,17 +1,17 @@
 package com.valleon.applyforme.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "Professional")
 public class Professional {
 
@@ -20,22 +20,42 @@ public class Professional {
     private long id;
 
     @JoinColumn(name = "user_id")
-    @OneToOne (fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Member member;
 
     @Column(name = "available_for_interview", nullable = false)
     private boolean availableForInterview;
 
-    @OneToOne(mappedBy = "professional")
-    private Applier applier;
+    @Column(name = "linkedin_link")
+    private String linkedinLink;
 
-    @OneToOne(mappedBy = "professional")
-    private ProfessionalAttachment professionalAttachment;
+    @Column(name = "twitter_link")
+    private String twitterLink;
 
-    @OneToOne(mappedBy = "professional")
-    private ProfessionalMetadata professionalMetadata;
+    @Column(name = "facebook_link")
+    private String facebookLink;
 
-    @OneToOne(fetch = FetchType.LAZY, mappedBy = "professional")
-    private Submission submission;
+    @Column(name = "instagram_link")
+    private String instagramLink;
+
+    @Column(name = "hobbies")
+    private String hobbies;
+
+    @Column(name = "other_link")
+    private String otherLink;
+
+    @Column(name = "other_link_2")
+    private String otherLink2;
+
+    @Column(name = "other_link_3")
+    private String otherLink3;
+
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<ProfessionalProfile> professionalProfiles = new HashSet<>();
+
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "professional")
+    private Set<Submission> submissions = new HashSet<>();
 
 }
