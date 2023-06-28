7d299a80e66e4b5bcb43219fd802c2ca5adb5c3d
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2023-06-28 20:04:26 +0100

    Created controller for Applicants to get their detaiils

--- src/main/java/com/valleon/applyforme/controllers/ApplicantController.java
+++ src/main/java/com/valleon/applyforme/controllers/ApplicantController.java
@@ -1,20 +1,24 @@
 package com.valleon.applyforme.controllers;
 
 import com.valleon.applyforme.model.domain.Member;
-import lombok.var;
-import org.springframework.http.MediaType;
+import com.valleon.applyforme.services.ApplicantService;
+import com.valleon.applyforme.utilities.CurrentUserUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping(value = "applicant", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class ApplicantController {
 
+    private ApplicantService applicantService;
 
     @GetMapping("/details")
     public Member getMyDetails(Long id){
-        var
+        var authenticatedUser = CurrentUserUtil.getCurrentUser();
+        assert authenticatedUser != null;
+        return applicantService.getDetails(authenticatedUser.getId());
 
     }
 }
